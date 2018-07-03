0acf6d6f03aa45d233b741abcbd11da31b0c8c99
iepoch <staclem@live.com>
2018-07-03 12:04:26 -0700

    creating new quiz updated js with  results

--- js/index.js
+++ js/index.js
@@ -1,32 +1,61 @@
 /*https://www.sitepoint.com/simple-javascript-quiz/
 view-source:http://webbness.net/index.html
 */
+(function() {
+  
 
-const quizContainer = document.getElementById('quiz');
-const resultsContainer= document.getElementById('results');
-function(){
-const questions = [{
-    question: "Do you have cash in your pocket?",
-    options: {1: "Yes", 2: "Kind Of",3: "No",4: "You must be kidding?"},
-    answer: [1,2,3,4]
-}
+  function buildQuiz() {
+    const  output = [];
+    question.forEach((currentQuestion, questionNumber) => {
+       
+        const options = [];
 
-]}
-
-function buildQuiz(){
-const output =[]; 
-const options = [];
-questions.forEach((currentQuestion, questionNumber)=>{
- 
-    for(number in currentQuestion.options)
-    options.push(
-       ` <label>
+      for (number in currentQuestion.options) {
+        options.push(` <label>
             <input type="radio" name="question${questionNumber}" value="${number}">
                 ${number} :
               ${currentQuestion.options[number]}
-           </label>`
-     );
- })
- 
- 
-}
+           </label>`);
+      }
+      -+number;
+
+      output.push(
+        `<div class= "question"> ${currentQuestion.question} </div>
+     <div class="options">${options.join("")}</div>
+     `
+      );
+    });
+      quizContainer.innerHTML = output.join("");
+  }
+
+
+  function showResults() {
+    const answerContainers = quizContainer.querySelectorAll(".options");
+
+    let totalNum = 0;
+
+    question.forEach((currentQuestion, questionNumber) => {
+      const answerContainer = answerContainers[questionNumber];
+      const selector = "input [type=radio" + totalNum + "]:checked";
+      const userAnswer = (answerContainer.querySelector(selector) || {}).value;
+
+   totalNum += userAnswer;
+
+    });
+  }
+
+    const quizContainer = document.getElementById("quiz");
+    const resultsContainer = document.getElementById("results");
+  const question = [
+    {
+      question: "Do you have cash in your pocket?",
+      options: { 1: "Yes", 2: "Kind Of", 3: "No", 4: "You must be kidding?" }
+    },
+    {
+      question: "Do you have cash in your pocket?",
+      options: { 1: "Yes", 2: "Kind Of", 3: "No", 4: "You must be kidding?" }
+    }
+  ];
+
+  buildQuiz();
+})();
